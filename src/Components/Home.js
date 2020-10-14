20ecd6bf69d2c51fbef266fc6dfa24894580ad85
yatharth <yatharthverma070@gmail.com>
2020-10-14 11:15:11 +0530

    change layout of front page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,47 @@
 import React from 'react';
+import plus from './../Assets/plus.png';
+import contactIcon from './../Assets/contact-books.png';
+import AllContacts from './AllContacts';
+import CreateForm from './CreateForm';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom"; 
 
 const Home = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
-        <>
-            <div className = "left-sidebar">
-                {/* navigation links  */}
-                <header className = "projectTitle">
-                    <span className = "span1">Manage</span>
-                    <span className = "span2">Contacts</span>
+        <Router>
+            <aside className="menu px-5 mt-3">
+                <h3 className = "subtitle">
+                    Manage
+                </h3>
+                <h1 className="title">
+                    Contacts
+                </h1>
 
-                    <button className = "btn btn-primary mt-3"><i className="fas fa-home"></i>Create Contact</button>
-                </header>
-                <main className = "nav-links">
-
-                </main>
+                <p className = "menu-label">
+                    General
+                </p>
+                <ul className = "menu-list">
+                    <li><a href="/" className = "button is-link mb-3">Contacts</a></li>
+                    <li><a href="/user" className="button is-link">Create Contact</a></li>
+                </ul>
+            </aside>
+            <div className="container px-5 py-4">
+            <Switch>
+                <Route exact path="/">
+                    <AllContacts/>
+                </Route>
+                <Route exact path="/user">
+                    <CreateForm />
+                </Route>
+            </Switch>
             </div>
-            <div className = "right-sidebar">
-
-            </div>
-        </>
+        </Router>
     )
 }
 
